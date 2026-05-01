9d03851c02951d0cd53f9b357a17e93ea4a59758
Wael <wael.noori122@gmail.com>
2026-05-01 19:53:10 +0300

    Remove seed configuration from package.json and add it to prisma.config.ts

--- cmps350-team5/prisma.config.ts
+++ cmps350-team5/prisma.config.ts
@@ -2,6 +2,9 @@ import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
   schema: './prisma/schema.prisma',
+  migrations: {
+    seed: 'bun ./prisma/seed.js',
+  },
   datasource: {
     url: env('DATABASE_URL'),
   },
