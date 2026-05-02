851d9a8f773052b25533ebec2b2ffe7bf7e7d7e0
Wael <wael.noori122@gmail.com>
2026-05-03 01:56:05 +0300

    fix: update seed command to use 'bun' instead of 'npx tsx'

--- cmps350-team5/prisma.config.mjs
+++ cmps350-team5/prisma.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'prisma/config'
 export default defineConfig({
   schema: './prisma/schema.prisma',
   migrations: {
-    seed: 'npx tsx ./prisma/seed.js',
+    seed: 'bun ./prisma/seed.js',
   },
   datasource: {
     url: process.env.DATABASE_URL,
